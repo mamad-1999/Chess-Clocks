0d545e1b4298d5c949612c4ac7f1a67c86fb37b6
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-05-05 19:09:54 +0330

    fit: complete initial ToolBar UI 🚀

--- src/app/components/NumberBox.tsx
+++ src/app/components/NumberBox.tsx
@@ -7,7 +7,7 @@ const NumberBox = ({ color }: PropColor) => {
     return (
         <div className={`w-full md:w-1/2 ${color} md:h-screen h-1/2 flex items-center justify-center`
         }>
-            <h3 className="text-8xl font-bold text-zinc-900">
+            <h3 className="text-8xl font-semibold text-zinc-900">
                 3:00
             </h3>
         </div >
