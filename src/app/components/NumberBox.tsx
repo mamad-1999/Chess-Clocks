6d8be77146cb85719135b236fd57981897fb38b2
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-05-10 18:20:37 +0330

    style: change moveCount Style

--- src/app/components/NumberBox.tsx
+++ src/app/components/NumberBox.tsx
@@ -24,7 +24,7 @@ const NumberBox = (
             onClick={() => click()}
             className={`w-full md:w-1/2 ${color} md:h-screen h-1/2 flex items-center justify-center cursor-pointer select-none relative`
             }>
-            <span className="absolute top-3 right-3">Move = {moveCount}</span>
+            <span className={`absolute md:top-3 md:right-3 ${mobileStyle ? "bottom-3 right-3" : "top-3 right-3"} text-zinc-800 ${mobileStyle}`}>Move = {moveCount}</span>
             <h3 className={`text-8xl md:text-8xl text-center font-semibold ${color === "bg-stone-500" ? "text-zinc-900" : "text-white"} ${mobileStyle}`}>
                 {formatTime(time).substring(-1, 4)}
                 <span className="text-5xl">{formatTime(time).substring(5, 9)}</span>
