b076f3a25086ea4eb26298c6de853de7c9ee0cae
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-05-20 11:48:13 +0330

    style: change style and content of modal {finish}

--- src/app/components/Modal.tsx
+++ src/app/components/Modal.tsx
@@ -7,6 +7,7 @@ import { getLocalStorageItem } from "../../../util/storage";
 import { clearTime } from "../../../util/clearTime";
 export default function Modal() {
     const modalState = useAppSelector(state => state.modal)
+    const setting = useAppSelector(state => state.setting)
     const dispatch = useAppDispatch()
 
     const resetTimer = () => {
@@ -19,38 +20,33 @@ export default function Modal() {
             {modalState.showModal ? (
                 <>
                     <div
-                        className="justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none"
+                        className={`justify-center items-center flex overflow-x-hidden overflow-y-auto fixed inset-0 z-50 outline-none focus:outline-none ${modalState.showModal && "bg-stone-500/70"}`}
                     >
                         <div className="relative w-auto my-6 mx-auto max-w-3xl">
                             {/*content*/}
-                            <div className="border-0 rounded-lg shadow-lg relative flex flex-col w-full bg-white outline-none focus:outline-none">
-                                {/*header*/}
-                                <div className="flex items-start justify-between p-5 border-b border-solid border-slate-200 rounded-t">
-                                    <h3 className="text-3xl font-semibold">
-                                        Modal Title
-                                    </h3>
-                                </div>
+                            <div className="md:w-96 w-80 border-0 rounded-lg shadow-lg relative flex flex-col bg-zinc-800 outline-none focus:outline-none">
+
                                 {/*body*/}
-                                <div className="relative p-6 flex-auto">
-                                    <p className="my-4 text-slate-500 text-lg leading-relaxed">
-                                        I always felt like I could do anything.
+                                <div className="relative p-4 flex-auto">
+                                    <p className="my-2 text-white text-lg leading-relaxed">
+                                        Reset the clock?
                                     </p>
                                 </div>
                                 {/*footer*/}
-                                <div className="flex items-center justify-end p-6 border-t border-solid border-slate-200 rounded-b">
+                                <div className="flex gap-3 items-center justify-end py-4 px-6 rounded-b">
                                     <button
                                         className="text-red-500 background-transparent font-bold uppercase px-6 py-2 text-sm outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
                                         type="button"
                                         onClick={() => dispatch(closeModalHandler())}
                                     >
-                                        Close
+                                        No
                                     </button>
                                     <button
-                                        className="bg-emerald-500 text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
+                                        className={`${setting.themeColor} text-white active:bg-emerald-600 font-bold uppercase text-sm px-6 py-2 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150`}
                                         type="button"
                                         onClick={resetTimer}
                                     >
-                                        Save Changes
+                                        YES
                                     </button>
                                 </div>
                             </div>
