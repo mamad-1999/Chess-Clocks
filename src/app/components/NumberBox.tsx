be3481aec501e9ca3b371f75d0609c91f3c07d7c
mohammadyousefvand <www.mmhmdmm83@gmail.com>
2023-05-22 19:02:49 +0330

    fix: fix showing singleTime modal in play === true

--- src/app/components/NumberBox.tsx
+++ src/app/components/NumberBox.tsx
@@ -1,3 +1,6 @@
+"use client"
+
+import { useAppSelector } from "@/redux/hook"
 import { formatTime } from "../../../util/formatTime"
 
 type PropColor = {
@@ -20,13 +23,14 @@ const NumberBox = (
         changeTime
     }: PropColor
 ) => {
+    const toolBarState = useAppSelector(state => state.toolBar)
     return (
         <div className="w-full flex  justify-center md:w-1/2 md:h-screen h-1/2 relative select-none">
-            <button onClick={() => changeTime()} className="absolute bottom-3">
+            {!toolBarState.playStatus ? <button onClick={() => changeTime()} className="absolute bottom-3">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-10">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 6h9.75M10.5 6a1.5 1.5 0 11-3 0m3 0a1.5 1.5 0 10-3 0M3.75 6H7.5m3 12h9.75m-9.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-3.75 0H7.5m9-6h3.75m-3.75 0a1.5 1.5 0 01-3 0m3 0a1.5 1.5 0 00-3 0m-9.75 0h9.75" />
                 </svg>
-            </button>
+            </button> : null}
             <button
                 disabled={playing}
                 onClick={() => click()}
